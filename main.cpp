284326f2a0a3f6ee43464aca1415dbf6719f38aa
Dima-Git <urmemoris@gmail.com>
2015-05-23 17:23:48 +0300

    Camera capturing test program

--- main.cpp
+++ main.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include "CameraImageStream.h"
 
 int main(int argc, char * argv[])
 {
-	std::cout << "Hello world!" << std::endl;
+	const cv::String windowName = "Window";
+	CameraImageStream stream(0);
+	cv::Mat frame;
+	
+	cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+	
+	bool running = true;
+	while (running) {
+		
+		stream.getImage(frame);
+		
+		cv::imshow(windowName, frame);
+		
+		switch (cv::waitKey(0)) {
+		case -1 :
+			break;
+		case 27 :
+			running = false;
+			break;
+		}
+	}
+	
 	return 0;
 }
