1c724f7114f2fee26c09aba418ffc39c33b7fe3a
Dima-Git <urmemoris@gmail.com>
2015-05-23 17:52:37 +0300

    Array stream test program

--- main.cpp
+++ main.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
 #include <opencv2/opencv.hpp>
-#include "CameraImageStream.h"
+#include "ArrayImageStream.h"
 
 int main(int argc, char * argv[])
 {
 	const cv::String windowName = "Window";
-	CameraImageStream stream(0);
+	std::vector<cv::Mat> images;
+	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.00);
+	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.25);
+	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.50);
+	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.75);
+	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 1.00);
+	ArrayImageStream stream(images);
 	cv::Mat frame;
 	
 	cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
 	
 	bool running = true;
-	while (running) {
-		
-		stream.getImage(frame);
+	while (running && stream.getImage(frame)) {
 		
 		cv::imshow(windowName, frame);
 		
