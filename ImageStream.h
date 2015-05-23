36b1edc9fb97b9cc6571f84f0e310940307c4cef
Dima-Git <urmemoris@gmail.com>
2015-05-23 17:21:49 +0300

    Added ImageStream interface for retrieving cv::Mat images

---
+++ ImageStream.h
@@ -0,0 +1,13 @@
+#ifndef _IMAGE_STREAM_H_
+#define _IMAGE_STREAM_H_
+
+#include <opencv2/opencv.hpp>
+
+class ImageStream {
+	
+public:
+	virtual bool getImage(cv::Mat & image) = 0;
+	
+};
+
+#endif // _IMAGE_STREAM_H_
