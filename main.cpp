37730804f83f4f1c57d9beee6720a27486e9c799
Dima-Git <urmemoris@gmail.com>
2015-05-24 03:08:21 +0300

    Camera config test program

--- main.cpp
+++ main.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include "ArrayImageStream.h"
+#include "ConfigLoader.h"
 
 int main(int argc, char * argv[])
 {
+	if ( argc == 1 ) {
+		std::cerr << "Config file not specified." << std::endl;
+		return 1;
+	}
+	
+	ImageStream * pstream = loadConfig(argv[1]);
+	
+	if ( !pstream ) {
+		std::cerr << "Config file '" << argv[1] << "' read error." << std::endl;
+		return 2;
+	}
+	
+	
 	const cv::String windowName = "Window";
 	std::vector<cv::Mat> images;
-	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.00);
-	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.25);
-	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.50);
-	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 0.75);
-	images.push_back(cv::Mat::ones(240, 320, CV_32FC1) * 1.00);
-	ArrayImageStream stream(images);
 	cv::Mat frame;
 	
 	cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
 	
 	bool running = true;
-	while (running && stream.getImage(frame)) {
+	while (running && pstream->getImage(frame)) {
 		
 		cv::imshow(windowName, frame);
 		
