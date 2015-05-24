b6597955470cf29303b3ad76778a0385cd6690f1
Dima-Git <urmemoris@gmail.com>
2015-05-24 16:55:25 +0300

    Difference Analyzer test program.

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include "ConfigLoader.h"
+#include "DifferenceAnalyzer.h"
 
 int main(int argc, char * argv[])
 {
-	if ( argc == 1 ) {
+	if ( argc < 2 ) {
 		std::cerr << "Config file not specified." << std::endl;
 		return 1;
 	}
@@ -15,19 +16,24 @@ int main(int argc, char * argv[])
 		return 2;
 	}
 	
-	
-	const cv::String windowName = "Window";
 	std::vector<cv::Mat> images;
 	cv::Mat frame;
+	pstream->getImage(frame);
+	DifferenceAnalyzer analyzer(frame, argv[2]);
 	
-	cv::namedWindow(windowName, cv::WINDOW_AUTOSIZE);
+	for ( int stage = 0 ; stage < DifferenceAnalyzer::STAGE_COUNT ; ++ stage )
+		cv::namedWindow(((std::stringstream&)(std::stringstream()<<stage)).str(), cv::WINDOW_AUTOSIZE);
 	
 	bool running = true;
 	while (running && pstream->getImage(frame)) {
 		
-		cv::imshow(windowName, frame);
+		analyzer.takeNextImage(frame);
+		for ( int stage = 0 ; stage < DifferenceAnalyzer::STAGE_COUNT ; ++ stage ) {
+			analyzer.getStage(frame, stage);
+			cv::imshow(((std::stringstream&)(std::stringstream()<<stage)).str(), frame);
+		}
 		
-		switch (cv::waitKey(0)) {
+		switch (cv::waitKey(100)) {
 		case -1 :
 			break;
 		case 27 :
